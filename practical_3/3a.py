c4d75454f9241702ec61054e97550aaa49a12948
poojayadav7 <pooja1105yadav@gmail.com>
2025-12-28 20:11:48 +0530

    implement Hebb’s rule.

---
+++ practical_3/3a.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+# First pattern
+x1 = np.array([1, 1, 1, -1, 1, -1, 1, 1, 1])
+
+# Second pattern
+x2 = np.array([1, 1, 1, 1, -1, 1, 1, 1, 1])
+
+# Initialize bias value
+b = 0
+
+# Define target
+y = np.array([1, -1])
+
+# Initialize weights
+wtold = np.zeros((9,))
+wtnew = np.zeros((9,))
+wtnew = wtnew.astype(int)
+wtold = wtold.astype(int)
+
+bais = 0
+
+print("First input with target = 1")
+for i in range(0, 9):
+    wtold[i] = wtold[i] + x1[i] * y[0]
+wtnew = wtold
+b = b + y[0]
+print("New weights =", wtnew)
+print("Bias value =", b)
+
+print("\nSecond input with target = -1")
+for i in range(0, 9):
+    wtnew[i] = wtold[i] + x2[i] * y[1]
+b = b + y[1]
+print("New weights =", wtnew)
+print("Bias value =", b)
+
